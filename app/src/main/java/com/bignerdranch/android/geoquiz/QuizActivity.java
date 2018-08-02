477a03f2c7f931240147d6bae076e0d9ca4816fd
wrb002 <37055512+wrb002@users.noreply.github.com>
2018-08-02 21:46:11 +1000

    got all mostly working

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -10,6 +10,7 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
